42617ad665f96a4aa94ac1f6bac4290ba8f8dcaf
Topeojewale <topeojewale@gmail.com>
2018-06-19 23:09:48 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+//components
+import Header from './components/header/Header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Header.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__list--item">
+                <a className="nav__brand" href="/">
+                  <img
+                    className="nav__brand--img"
+                    src="../img/logo.png"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li className="nav__list--item">
+                <a className="nav__list--item-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav__list--item">
+                <a className="nav__list--item-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav__list--item">
+                <a className="nav__list--item-link" href="#">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+}
+export default Header;
