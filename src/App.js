ecd9385e2f5d47a1be8519e02388813af24d3f7c
Topeojewale <topeojewale@gmail.com>
2018-06-20 07:42:41 +0100

    Added Banner component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 //components
 import Header from './components/header/Header'
+import Banner from './components/banner/Banner';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Banner/>
       </div>
     );
   }
