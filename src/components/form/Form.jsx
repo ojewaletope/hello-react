c996f8420cee618326e0148ad7f376dcb99119d0
Topeojewale <topeojewale@gmail.com>
2018-06-21 08:05:48 +0100

    Added functionalities to the stateful component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,26 +1,72 @@
 import React from "react";
-import './Form.css'
+import "./Form.css";
 
 class Form extends React.Component {
-  state = {};
+  state = {
+    data: {
+      name: "",
+      email: "",
+      message: ""
+    },
+    error: {}
+  };
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.data)
+    this.setState({
+      data:{
+        name: "",
+        email: '',
+        message: ''
+      }
+    })
+  };
   render() {
-    return <div className="form__container">
+    const { data } = this.state;
+    return (
+      <div className="form__container">
         <div className="form__heading">
           <h3>contact me for your web jobs</h3>
         </div>
         <form action="" className="contact__form">
           <div>
-            <input type="text" name="fullname" placeholder="Your full name" className="form__input" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Your full name"
+              className="form__input"
+              value={data.name}
+              onChange={this.onChange}
+            />
           </div>
           <div>
-            <input type="email" name="email" placeholder="e.g example@example.com" className="form__input" />
+            <input
+              type="email"
+              name="email"
+              placeholder="e.g example@example.com"
+              className="form__input"
+              value={data.email}
+              onChange={this.onChange}
+            />
           </div>
           <div>
-            <textarea className="form__textarea" placeholder="Your message" required />
+            <textarea
+              className="form__textarea"
+              placeholder="Your message"
+              required
+              name="message"
+              value={data.message}
+              onChange={this.onChange}
+            />
           </div>
-          <button className="form__btn">Send Me A Request</button>
+          <button className="form__btn" onClick={this.handleSubmit}>Send Me A Request</button>
         </form>
-      </div>;
+      </div>
+    );
   }
 }
 
